8d7719b61880f23c65ba102ae8b7ff06627da5df
wighawag <wighawag@gmail.com>
2022-07-09 17:56:25 +0100

    fixes, use CRON

--- server/src/index.ts
+++ server/src/index.ts
@@ -25,6 +25,9 @@ export async function handleRequest(
       case '/events/setup': // TODO should be guarded by admin authentication
       case '/events/list':
       case '/events/feed': // TODO should be guarded by admin authentication
+      case '/events/status':
+      case '/events/trigger-alarm': // TODO remove or admin gated
+      // case '/events/set-alarm-if-stuck': // TODO remove or admin gated
       case '/query':
         // we only need one DO so we use _GLOBAL_ id
         const namespace = env.ETHEREUM_EVENTS;
@@ -53,7 +56,42 @@ export async function handleRequest(
   }
 }
 
-const worker: ExportedHandler<Env> = { fetch: handleRequest };
+const worker: ExportedHandler<Env> = {
+  fetch: handleRequest,
+
+  // required to ensure alarm redundency
+  // => does not work actually
+
+  // scheduled: async (controller, env) => {
+  //   const namespace = env.ETHEREUM_EVENTS;
+  //   const DO = namespace.get(namespace.idFromName('_GLOBAL_'));
+  //   try {
+  //     const response = await DO.fetch(
+  //       new Request('http://localhost/events/set-alarm-if-stuck'),
+  //     );
+  //     console.log(`response: ${await response.text()}`);
+  //   } catch (e) {
+  //     console.error(`error`, e);
+  //   }
+  // },
+
+  // simply process on CRON
+  // we still have the alarm enabled, let see...
+  scheduled: async (controller, env) => {
+    const namespace = env.ETHEREUM_EVENTS;
+    const DO = namespace.get(namespace.idFromName('_GLOBAL_'));
+    try {
+      const response = await DO.fetch(
+        new Request('http://localhost/events/fetchLogsAndProcess'),
+      );
+      console.log(
+        `response: ${JSON.stringify(await response.json(), null, 2)}`,
+      );
+    } catch (e) {
+      console.error(`error`, e);
+    }
+  },
+};
 
 import { Room } from '../lib/do-verse/src/Room';
 Room.signatureTemplate = `Verifying account ownership of {owner} for Meebits owner access.`;
@@ -69,6 +107,12 @@ import { meebitAsset } from './meebits/utils';
 
 // export Durable Object Classes
 export { MeebitsRoom as Room };
+
+import { EthereumEventsDO } from '../lib/do-verse/lib/do-ethereum-events/src/EthereumEventsDO';
+EthereumEventsDO.alarm = {
+  interval: 15,
+  individualCall: true,
+};
 
 export { Blockchain as EthereumEventsDO } from '../lib/do-verse/src/Blockchain';
 
